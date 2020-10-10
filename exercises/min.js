7cdd26001c9a427d65909f3e3cc80e175a8e7087
Stella <stella.dmt@gmail.com>
2020-10-10 19:06:55 +0200

    resolved min

--- exercises/min.js
+++ exercises/min.js
@@ -11,6 +11,14 @@
 
 // Your code:
 
+function min (numOne, numTwo) {
+    if (numOne <= numTwo) {
+        return numOne;
+    } else {
+        return numTwo;
+    }
+}
+
 //* Begin of tests
 const assert = require('assert');
 
