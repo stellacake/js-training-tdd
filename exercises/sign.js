5d7f7de6714534a09ba4f496b06f86e89e8c9fb6
Stella <stella.dmt@gmail.com>
2020-10-10 19:11:24 +0200

    resolved sign

--- exercises/sign.js
+++ exercises/sign.js
@@ -12,6 +12,16 @@
 
 // Your code:
 
+function sign(num) {
+    if (num < 0) {
+        return -1;
+    } else if (num > 0) {
+        return 1;
+    } else if (num === 0) {
+        return 0;
+    }
+}
+
 //* Begin of tests
 const assert = require('assert');
 
